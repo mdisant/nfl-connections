2322cf3c923db482c755f425274eb56bd2a8fcb3
mdisant <mdisant@umich.edu>
2023-10-27 12:29:48 -0400

    lib folder files

---
+++ src/lib/constants.js
@@ -0,0 +1,2 @@
+export const GAME_TITLE = "NFL Connections";
+export const MAX_MISTAKES = 4;
---
+++ src/lib/data.js
@@ -0,0 +1,24 @@
+export const CONNECTION_GAMES = [
+    [
+        {
+            category: "Hall of Fame Quarterbacks",
+            words: ["Joe Montana", "Peyton Manning", "Brett Favre", "Dan Marino"],
+            difficulty: 1,
+        },
+        {
+            category: "Top 4 WRs in 2023 Draft",
+            words: ["Jaxon Smith-Njigba", "Quentin Johnson", "Zay Flowers", "Jordan Addison"],
+            difficulty: 2,
+        },
+        {
+            category: "Current NY Giants",
+            words: ["Daniel Jones", "Andrew Thomas", "Saquon Barkley", "Jalin Hyatt"],
+            difficulty: 3,
+        },
+        {
+            category: "Penn State Alumni",
+            words: ["Micah Parsons", "Jahan Dotson", "Miles Sanders", "Chris Godwin"],
+            difficulty: 4,
+        },
+    ],
+];
