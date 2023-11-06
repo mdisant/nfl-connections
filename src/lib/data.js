7adfa8219293e923e7b23f66efdf04389cf80bbd
mdisant <mdisant@umich.edu>
2023-11-06 16:28:55 -0500

    provide temporary data - need to source more

--- src/lib/data.js
+++ src/lib/data.js
@@ -67,4 +67,480 @@ export const CONNECTION_GAMES = [
             difficulty: 4,
         },
     ],
+    [
+        {
+            category: "Hall of Fame Quarterbacks",
+            words: ["Joe Montana", "Peyton Manning", "Brett Favre", "Dan Marino"],
+            difficulty: 1,
+        },
+        {
+            category: "Top 4 WRs in 2023 Draft",
+            words: ["Jaxon Smith-Njigba", "Quentin Johnson", "Zay Flowers", "Jordan Addison"],
+            difficulty: 2,
+        },
+        {
+            category: "Current NY Giants",
+            words: ["Daniel Jones", "Andrew Thomas", "Saquon Barkley", "Jalin Hyatt"],
+            difficulty: 3,
+        },
+        {
+            category: "Penn State Alumni",
+            words: ["Micah Parsons", "Jahan Dotson", "Miles Sanders", "Chris Godwin"],
+            difficulty: 4,
+        },
+    ],
+
+    [
+        {
+            category: "Detroit Lions Players",
+            words: ["Aidan Hutchinson", "Jameson Williams", "Amon-Ra St. Brown", "Jared Goff"],
+            difficulty: 1,
+        },
+        {
+            category: "NE Patriots Players",
+            words: ["Jakobi Meyers", "Mac Jones", "Christian Gonzalez", "Rhamondre Stevenson"],
+            difficulty: 2,
+        },
+        {
+            category: "Alabama Running Backs",
+            words: ["Najee Harris", "Damien Harris", "Josh Jacobs", "Jahmyr Gibbs"],
+            difficulty: 3,
+        },
+        {
+            category: "Michigan Defensive Players",
+            words: ["Dax Hill", "Jabrill Peppers", "Mazi Smith", "Jourdan Lewis"],
+            difficulty: 4,
+        },
+    ],
+
+    [
+        {
+            category: "Starting Tight Ends",
+            words: ["Kyle Pitts", "Darren Waller", "Travis Kelce", "Trey McBride"],
+            difficulty: 1,
+        },
+        {
+            category: "First Round Draft Picks in 2021",
+            words: ["Jaylen Waddle", "Patrick Surtain II", "Devonta Smith", "Najee Harris"],
+            difficulty: 2,
+        },
+        {
+            category: "Miami Wide Receivers",
+            words: ["Tyreek Hill", "Cedrick Wilson", "Braxton Berrios", "Durham Smythe"],
+            difficulty: 3,
+        },
+        {
+            category: "Super Bowl LIV Players",
+            words: ["Raheem Mostert", "Harrison Butker", "Jimmy Garappolo", "Richard Sherman"],
+            difficulty: 4,
+        },
+    ],
+    [
+        {
+            category: "Hall of Fame Quarterbacks",
+            words: ["Joe Montana", "Peyton Manning", "Brett Favre", "Dan Marino"],
+            difficulty: 1,
+        },
+        {
+            category: "Top 4 WRs in 2023 Draft",
+            words: ["Jaxon Smith-Njigba", "Quentin Johnson", "Zay Flowers", "Jordan Addison"],
+            difficulty: 2,
+        },
+        {
+            category: "Current NY Giants",
+            words: ["Daniel Jones", "Andrew Thomas", "Saquon Barkley", "Jalin Hyatt"],
+            difficulty: 3,
+        },
+        {
+            category: "Penn State Alumni",
+            words: ["Micah Parsons", "Jahan Dotson", "Miles Sanders", "Chris Godwin"],
+            difficulty: 4,
+        },
+    ],
+
+    [
+        {
+            category: "Detroit Lions Players",
+            words: ["Aidan Hutchinson", "Jameson Williams", "Amon-Ra St. Brown", "Jared Goff"],
+            difficulty: 1,
+        },
+        {
+            category: "NE Patriots Players",
+            words: ["Jakobi Meyers", "Mac Jones", "Christian Gonzalez", "Rhamondre Stevenson"],
+            difficulty: 2,
+        },
+        {
+            category: "Alabama Running Backs",
+            words: ["Najee Harris", "Damien Harris", "Josh Jacobs", "Jahmyr Gibbs"],
+            difficulty: 3,
+        },
+        {
+            category: "Michigan Defensive Players",
+            words: ["Dax Hill", "Jabrill Peppers", "Mazi Smith", "Jourdan Lewis"],
+            difficulty: 4,
+        },
+    ],
+
+    [
+        {
+            category: "Starting Tight Ends",
+            words: ["Kyle Pitts", "Darren Waller", "Travis Kelce", "Trey McBride"],
+            difficulty: 1,
+        },
+        {
+            category: "First Round Draft Picks in 2021",
+            words: ["Jaylen Waddle", "Patrick Surtain II", "Devonta Smith", "Najee Harris"],
+            difficulty: 2,
+        },
+        {
+            category: "Miami Wide Receivers",
+            words: ["Tyreek Hill", "Cedrick Wilson", "Braxton Berrios", "Durham Smythe"],
+            difficulty: 3,
+        },
+        {
+            category: "Super Bowl LIV Players",
+            words: ["Raheem Mostert", "Harrison Butker", "Jimmy Garappolo", "Richard Sherman"],
+            difficulty: 4,
+        },
+    ],
+    [
+        {
+            category: "Hall of Fame Quarterbacks",
+            words: ["Joe Montana", "Peyton Manning", "Brett Favre", "Dan Marino"],
+            difficulty: 1,
+        },
+        {
+            category: "Top 4 WRs in 2023 Draft",
+            words: ["Jaxon Smith-Njigba", "Quentin Johnson", "Zay Flowers", "Jordan Addison"],
+            difficulty: 2,
+        },
+        {
+            category: "Current NY Giants",
+            words: ["Daniel Jones", "Andrew Thomas", "Saquon Barkley", "Jalin Hyatt"],
+            difficulty: 3,
+        },
+        {
+            category: "Penn State Alumni",
+            words: ["Micah Parsons", "Jahan Dotson", "Miles Sanders", "Chris Godwin"],
+            difficulty: 4,
+        },
+    ],
+
+    [
+        {
+            category: "Detroit Lions Players",
+            words: ["Aidan Hutchinson", "Jameson Williams", "Amon-Ra St. Brown", "Jared Goff"],
+            difficulty: 1,
+        },
+        {
+            category: "NE Patriots Players",
+            words: ["Jakobi Meyers", "Mac Jones", "Christian Gonzalez", "Rhamondre Stevenson"],
+            difficulty: 2,
+        },
+        {
+            category: "Alabama Running Backs",
+            words: ["Najee Harris", "Damien Harris", "Josh Jacobs", "Jahmyr Gibbs"],
+            difficulty: 3,
+        },
+        {
+            category: "Michigan Defensive Players",
+            words: ["Dax Hill", "Jabrill Peppers", "Mazi Smith", "Jourdan Lewis"],
+            difficulty: 4,
+        },
+    ],
+
+    [
+        {
+            category: "Starting Tight Ends",
+            words: ["Kyle Pitts", "Darren Waller", "Travis Kelce", "Trey McBride"],
+            difficulty: 1,
+        },
+        {
+            category: "First Round Draft Picks in 2021",
+            words: ["Jaylen Waddle", "Patrick Surtain II", "Devonta Smith", "Najee Harris"],
+            difficulty: 2,
+        },
+        {
+            category: "Miami Wide Receivers",
+            words: ["Tyreek Hill", "Cedrick Wilson", "Braxton Berrios", "Durham Smythe"],
+            difficulty: 3,
+        },
+        {
+            category: "Super Bowl LIV Players",
+            words: ["Raheem Mostert", "Harrison Butker", "Jimmy Garappolo", "Richard Sherman"],
+            difficulty: 4,
+        },
+    ],
+    [
+        {
+            category: "Hall of Fame Quarterbacks",
+            words: ["Joe Montana", "Peyton Manning", "Brett Favre", "Dan Marino"],
+            difficulty: 1,
+        },
+        {
+            category: "Top 4 WRs in 2023 Draft",
+            words: ["Jaxon Smith-Njigba", "Quentin Johnson", "Zay Flowers", "Jordan Addison"],
+            difficulty: 2,
+        },
+        {
+            category: "Current NY Giants",
+            words: ["Daniel Jones", "Andrew Thomas", "Saquon Barkley", "Jalin Hyatt"],
+            difficulty: 3,
+        },
+        {
+            category: "Penn State Alumni",
+            words: ["Micah Parsons", "Jahan Dotson", "Miles Sanders", "Chris Godwin"],
+            difficulty: 4,
+        },
+    ],
+
+    [
+        {
+            category: "Detroit Lions Players",
+            words: ["Aidan Hutchinson", "Jameson Williams", "Amon-Ra St. Brown", "Jared Goff"],
+            difficulty: 1,
+        },
+        {
+            category: "NE Patriots Players",
+            words: ["Jakobi Meyers", "Mac Jones", "Christian Gonzalez", "Rhamondre Stevenson"],
+            difficulty: 2,
+        },
+        {
+            category: "Alabama Running Backs",
+            words: ["Najee Harris", "Damien Harris", "Josh Jacobs", "Jahmyr Gibbs"],
+            difficulty: 3,
+        },
+        {
+            category: "Michigan Defensive Players",
+            words: ["Dax Hill", "Jabrill Peppers", "Mazi Smith", "Jourdan Lewis"],
+            difficulty: 4,
+        },
+    ],
+
+    [
+        {
+            category: "Starting Tight Ends",
+            words: ["Kyle Pitts", "Darren Waller", "Travis Kelce", "Trey McBride"],
+            difficulty: 1,
+        },
+        {
+            category: "First Round Draft Picks in 2021",
+            words: ["Jaylen Waddle", "Patrick Surtain II", "Devonta Smith", "Najee Harris"],
+            difficulty: 2,
+        },
+        {
+            category: "Miami Wide Receivers",
+            words: ["Tyreek Hill", "Cedrick Wilson", "Braxton Berrios", "Durham Smythe"],
+            difficulty: 3,
+        },
+        {
+            category: "Super Bowl LIV Players",
+            words: ["Raheem Mostert", "Harrison Butker", "Jimmy Garappolo", "Richard Sherman"],
+            difficulty: 4,
+        },
+    ],
+    [
+        {
+            category: "Hall of Fame Quarterbacks",
+            words: ["Joe Montana", "Peyton Manning", "Brett Favre", "Dan Marino"],
+            difficulty: 1,
+        },
+        {
+            category: "Top 4 WRs in 2023 Draft",
+            words: ["Jaxon Smith-Njigba", "Quentin Johnson", "Zay Flowers", "Jordan Addison"],
+            difficulty: 2,
+        },
+        {
+            category: "Current NY Giants",
+            words: ["Daniel Jones", "Andrew Thomas", "Saquon Barkley", "Jalin Hyatt"],
+            difficulty: 3,
+        },
+        {
+            category: "Penn State Alumni",
+            words: ["Micah Parsons", "Jahan Dotson", "Miles Sanders", "Chris Godwin"],
+            difficulty: 4,
+        },
+    ],
+
+    [
+        {
+            category: "Detroit Lions Players",
+            words: ["Aidan Hutchinson", "Jameson Williams", "Amon-Ra St. Brown", "Jared Goff"],
+            difficulty: 1,
+        },
+        {
+            category: "NE Patriots Players",
+            words: ["Jakobi Meyers", "Mac Jones", "Christian Gonzalez", "Rhamondre Stevenson"],
+            difficulty: 2,
+        },
+        {
+            category: "Alabama Running Backs",
+            words: ["Najee Harris", "Damien Harris", "Josh Jacobs", "Jahmyr Gibbs"],
+            difficulty: 3,
+        },
+        {
+            category: "Michigan Defensive Players",
+            words: ["Dax Hill", "Jabrill Peppers", "Mazi Smith", "Jourdan Lewis"],
+            difficulty: 4,
+        },
+    ],
+
+    [
+        {
+            category: "Starting Tight Ends",
+            words: ["Kyle Pitts", "Darren Waller", "Travis Kelce", "Trey McBride"],
+            difficulty: 1,
+        },
+        {
+            category: "First Round Draft Picks in 2021",
+            words: ["Jaylen Waddle", "Patrick Surtain II", "Devonta Smith", "Najee Harris"],
+            difficulty: 2,
+        },
+        {
+            category: "Miami Wide Receivers",
+            words: ["Tyreek Hill", "Cedrick Wilson", "Braxton Berrios", "Durham Smythe"],
+            difficulty: 3,
+        },
+        {
+            category: "Super Bowl LIV Players",
+            words: ["Raheem Mostert", "Harrison Butker", "Jimmy Garappolo", "Richard Sherman"],
+            difficulty: 4,
+        },
+    ],
+    [
+        {
+            category: "Hall of Fame Quarterbacks",
+            words: ["Joe Montana", "Peyton Manning", "Brett Favre", "Dan Marino"],
+            difficulty: 1,
+        },
+        {
+            category: "Top 4 WRs in 2023 Draft",
+            words: ["Jaxon Smith-Njigba", "Quentin Johnson", "Zay Flowers", "Jordan Addison"],
+            difficulty: 2,
+        },
+        {
+            category: "Current NY Giants",
+            words: ["Daniel Jones", "Andrew Thomas", "Saquon Barkley", "Jalin Hyatt"],
+            difficulty: 3,
+        },
+        {
+            category: "Penn State Alumni",
+            words: ["Micah Parsons", "Jahan Dotson", "Miles Sanders", "Chris Godwin"],
+            difficulty: 4,
+        },
+    ],
+
+    [
+        {
+            category: "Detroit Lions Players",
+            words: ["Aidan Hutchinson", "Jameson Williams", "Amon-Ra St. Brown", "Jared Goff"],
+            difficulty: 1,
+        },
+        {
+            category: "NE Patriots Players",
+            words: ["Jakobi Meyers", "Mac Jones", "Christian Gonzalez", "Rhamondre Stevenson"],
+            difficulty: 2,
+        },
+        {
+            category: "Alabama Running Backs",
+            words: ["Najee Harris", "Damien Harris", "Josh Jacobs", "Jahmyr Gibbs"],
+            difficulty: 3,
+        },
+        {
+            category: "Michigan Defensive Players",
+            words: ["Dax Hill", "Jabrill Peppers", "Mazi Smith", "Jourdan Lewis"],
+            difficulty: 4,
+        },
+    ],
+
+    [
+        {
+            category: "Starting Tight Ends",
+            words: ["Kyle Pitts", "Darren Waller", "Travis Kelce", "Trey McBride"],
+            difficulty: 1,
+        },
+        {
+            category: "First Round Draft Picks in 2021",
+            words: ["Jaylen Waddle", "Patrick Surtain II", "Devonta Smith", "Najee Harris"],
+            difficulty: 2,
+        },
+        {
+            category: "Miami Wide Receivers",
+            words: ["Tyreek Hill", "Cedrick Wilson", "Braxton Berrios", "Durham Smythe"],
+            difficulty: 3,
+        },
+        {
+            category: "Super Bowl LIV Players",
+            words: ["Raheem Mostert", "Harrison Butker", "Jimmy Garappolo", "Richard Sherman"],
+            difficulty: 4,
+        },
+    ],
+    [
+        {
+            category: "Hall of Fame Quarterbacks",
+            words: ["Joe Montana", "Peyton Manning", "Brett Favre", "Dan Marino"],
+            difficulty: 1,
+        },
+        {
+            category: "Top 4 WRs in 2023 Draft",
+            words: ["Jaxon Smith-Njigba", "Quentin Johnson", "Zay Flowers", "Jordan Addison"],
+            difficulty: 2,
+        },
+        {
+            category: "Current NY Giants",
+            words: ["Daniel Jones", "Andrew Thomas", "Saquon Barkley", "Jalin Hyatt"],
+            difficulty: 3,
+        },
+        {
+            category: "Penn State Alumni",
+            words: ["Micah Parsons", "Jahan Dotson", "Miles Sanders", "Chris Godwin"],
+            difficulty: 4,
+        },
+    ],
+
+    [
+        {
+            category: "Detroit Lions Players",
+            words: ["Aidan Hutchinson", "Jameson Williams", "Amon-Ra St. Brown", "Jared Goff"],
+            difficulty: 1,
+        },
+        {
+            category: "NE Patriots Players",
+            words: ["Jakobi Meyers", "Mac Jones", "Christian Gonzalez", "Rhamondre Stevenson"],
+            difficulty: 2,
+        },
+        {
+            category: "Alabama Running Backs",
+            words: ["Najee Harris", "Damien Harris", "Josh Jacobs", "Jahmyr Gibbs"],
+            difficulty: 3,
+        },
+        {
+            category: "Michigan Defensive Players",
+            words: ["Dax Hill", "Jabrill Peppers", "Mazi Smith", "Jourdan Lewis"],
+            difficulty: 4,
+        },
+    ],
+
+    [
+        {
+            category: "Starting Tight Ends",
+            words: ["Kyle Pitts", "Darren Waller", "Travis Kelce", "Trey McBride"],
+            difficulty: 1,
+        },
+        {
+            category: "First Round Draft Picks in 2021",
+            words: ["Jaylen Waddle", "Patrick Surtain II", "Devonta Smith", "Najee Harris"],
+            difficulty: 2,
+        },
+        {
+            category: "Miami Wide Receivers",
+            words: ["Tyreek Hill", "Cedrick Wilson", "Braxton Berrios", "Durham Smythe"],
+            difficulty: 3,
+        },
+        {
+            category: "Super Bowl LIV Players",
+            words: ["Raheem Mostert", "Harrison Butker", "Jimmy Garappolo", "Richard Sherman"],
+            difficulty: 4,
+        },
+    ],
 ];
